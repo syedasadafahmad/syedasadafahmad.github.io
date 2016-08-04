2038eeaceab1463dbd8c35d2671cd6ab9d44baee
Ali Sheikh <alisheikh@alisheikh.Home>
2016-08-04 16:49:42 +0500

    Initial Commit

---
+++ scripts/init-slider-4.js
@@ -0,0 +1,11 @@
+$(function($) {
+    "use strict";
+
+    // for backstretch slider
+    $(".slideshow-div").backstretch([
+        "images/demo-img-5.jpg",
+        "images/demo-img-4.jpg",
+        "images/bg-img-1.jpg"
+    ], {duration: 3000, fade: 750});
+
+});
